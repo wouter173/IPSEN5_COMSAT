4143f78ee32e5e08b9b1289033467a03aced0ac9
Wouter de Bruijn <wouter@debruijn.dev>
2024-05-06 11:27:52 +0200

    Wouter/restructure (#1)
    * chore: moved angular app to own directory
    
    * feat: setup springboot
    
    * feat: update gitignore
    
    * feat: restructure complete

---
+++ comsat/src/test/java/nl/codefusion/comsat/ComsatApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.codefusion.comsat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
