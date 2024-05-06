690c88e3379ca28a86e4ef24a63a1efb11ad9760
wouter <wouter@debruijn.dev>
2024-05-06 11:14:15 +0200

    feat: setup springboot

---
+++ comsat/src/main/java/nl/codefusion/comsat/ComsatApplication.java
@@ -0,0 +1,13 @@
+package nl.codefusion.comsat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComsatApplication.class, args);
+    }
+
+}
