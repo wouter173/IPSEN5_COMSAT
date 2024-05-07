15e97c3ba5bcb6d11bfaf0ecf7c1930579895b91
Ruben Colijn <58084526+Draakje10@users.noreply.github.com>
2024-05-07 12:48:57 +0200

    Templates (#3)
    * Feat: added a wysiwyg
    
    * Feat: Templates can be read from the TemplateList and will be shown, Templates also have translations now
    
    * Feat: can now append to and save to template array

---
+++ untitled/src/main/java/org/example/FlyWithoutWings.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FlyWithoutWings implements IFlyBehaviour{
+
+    @Override
+    public void fly() {
+        System.out.println("Help mom i dont have wings!\n");
+    }
+}
---
+++ web/src/app/interfaces/language.ts
@@ -0,0 +1,5 @@
+export interface Language {
+  dutch: string;
+  english: string;
+  spanish: string;
+}
