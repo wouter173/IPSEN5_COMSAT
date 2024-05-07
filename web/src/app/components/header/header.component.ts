41cbc55938165974ed8d7b23b68e835ac2235eac
S1150223 <s1150223@student.hsleiden.nl>
2024-05-08 00:57:20 +0200

    feat: added dynamically switching text in the red bar

--- web/src/app/components/header/header.component.ts
+++ web/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router, Event as Event_2} from "@angular/router";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  currentPage: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter((event: Event_2): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: Event_2) => {
+      if (event instanceof NavigationEnd) {
+        let page = event.urlAfterRedirects;
+        page = page.replace('/', '');
+        this.currentPage = page.charAt(0).toUpperCase() + page.slice(1);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
