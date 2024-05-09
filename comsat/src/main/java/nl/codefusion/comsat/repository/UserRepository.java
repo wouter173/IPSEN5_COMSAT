3a7a9d7429e3b104c4c4aedc782259603ee3ff17
wouter <wouter@debruijn.dev>
2024-05-09 13:03:20 +0200

    feat: added user model

---
+++ comsat/src/main/java/nl/codefusion/comsat/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package nl.codefusion.comsat.repository;
+
+import nl.codefusion.comsat.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByUsername(String username);
+}
---
+++ comsat/src/main/java/nl/codefusion/comsat/service/UserDetailsImplService.java
@@ -0,0 +1,34 @@
+package nl.codefusion.comsat.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import nl.codefusion.comsat.models.UserModel;
+import nl.codefusion.comsat.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsImplService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return UserModel.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().stream().map(role -> "ROLE_" + role).collect(Collectors.toSet()))
+                .build();
+
+    }
+}
