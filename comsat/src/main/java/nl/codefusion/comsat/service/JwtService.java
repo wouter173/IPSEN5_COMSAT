127d5dacac4107828f8269780886781a25eee437
wouter <wouter@debruijn.dev>
2024-05-09 13:03:43 +0200

    feat: added jwt authentication and authorization with spring security

---
+++ comsat/src/main/java/nl/codefusion/comsat/service/JwtService.java
@@ -0,0 +1,55 @@
+package nl.codefusion.comsat.service;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    @SuppressWarnings("FieldCanBeLocal")
+    private final long expiration = 86_400_000;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private SecretKeySpec getSigningKey() {
+        byte[] keyBytes = Base64.getEncoder().encode(secretKey.getBytes());
+        return new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256) // updated method signature
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
