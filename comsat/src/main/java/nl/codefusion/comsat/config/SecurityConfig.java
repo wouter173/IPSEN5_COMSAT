9793be0ab52b60b15232af4f163a75ea5a99c5a0
Wouter de Bruijn <wouter@debruijn.dev>
2024-05-09 14:45:50 +0200

    Wouter/auth (#11)
    * feat: added user model
    
    * feat: added jwt authentication and authorization with spring security
    
    * feat: added seeder
    
    * chore: refactored HealthCheckController
    
    * chore: update example properties

---
+++ comsat/src/main/java/nl/codefusion/comsat/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package nl.codefusion.comsat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(JwtRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
--- comsat/src/main/java/nl/codefusion/comsat/controller/HealthCheckController.java
+++ comsat/src/main/java/nl/codefusion/comsat/controller/HealthCheckController.java
@@ -1,14 +1,16 @@
 package nl.codefusion.comsat.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/healthcheck")
 @RequiredArgsConstructor
-public class healthcheck {
-    @RequestMapping
+public class HealthCheckController {
+
+    @GetMapping
     public String getHealthcheck() {
         return "OK";
     }
