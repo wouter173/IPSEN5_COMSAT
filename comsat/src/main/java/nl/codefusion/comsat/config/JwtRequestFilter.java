d12a5309da55b5d987f7245c341a1e2605cb627e
wouter <wouter@debruijn.dev>
2024-05-09 13:03:43 +0200

    feat: added jwt authentication and authorization with spring security

---
+++ comsat/src/main/java/nl/codefusion/comsat/config/JwtRequestFilter.java
@@ -0,0 +1,52 @@
+package nl.codefusion.comsat.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import nl.codefusion.comsat.service.JwtService;
+import nl.codefusion.comsat.service.UserDetailsImplService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsImplService userDetailsImplService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain chain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        String jwt = null;
+        String username = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtService.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsImplService.loadUserByUsername(username);
+
+            if (jwtService.isTokenValid(jwt)) {
+                var authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+
+}
---
+++ comsat/src/main/java/nl/codefusion/comsat/controller/AuthController.java
@@ -0,0 +1,39 @@
+package nl.codefusion.comsat.controller;
+
+import lombok.RequiredArgsConstructor;
+import nl.codefusion.comsat.service.JwtService;
+import nl.codefusion.comsat.service.UserDetailsImplService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserDetailsImplService userDetailsImplService;
+
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        UserDetails userDetails = userDetailsImplService.loadUserByUsername(username);
+        String token = jwtService.generateToken(userDetails.getUsername());
+
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+}
