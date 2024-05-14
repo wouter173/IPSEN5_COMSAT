e6888a364208fdb0afa3ce07619e83bc0af3e27f
RoKost14 <s1135548@student.hsleiden.nl>
2024-05-14 11:00:31 +0200

    Refactor: hasPermission overload

--- comsat/src/main/java/nl/codefusion/comsat/service/PermissionService.java
+++ comsat/src/main/java/nl/codefusion/comsat/service/PermissionService.java
@@ -1,9 +1,10 @@
 package nl.codefusion.comsat.service;
 
+import nl.codefusion.comsat.config.Permission;
 import nl.codefusion.comsat.models.Role;
 
 public class PermissionService {
-    public boolean hasPermission(Role role, int neededPermission){
-        return(role.getPermissions() & neededPermission) == neededPermission;
+    public boolean hasPermission(Role role, Permission neededPermission){
+        return (role.getPermissions() & neededPermission.getValue()) == neededPermission.getValue();
     }
 }
