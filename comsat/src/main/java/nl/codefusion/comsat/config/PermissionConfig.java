38d21fd6d4a308b2e256b92d7a918d81e8df414c
RoKost14 <s1135548@student.hsleiden.nl>
2024-05-16 21:39:13 +0200

    fix: deleted obsolete files

--- comsat/src/main/java/nl/codefusion/comsat/config/PermissionConfig.java
+++
@@ -1,9 +0,0 @@
-package nl.codefusion.comsat.config;
-
-public class PermissionConfig {
-    public static final int CREATE_TEMPLATE = 1;
-    public static final int READ_TEMPLATE = 2;
-    public static final int EDIT_TEMPLATE = 4;
-    public static final int MANAGE_USERS = 8;
-
-}
