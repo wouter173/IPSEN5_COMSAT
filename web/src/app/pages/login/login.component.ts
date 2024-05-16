85bcf7a5f00ea2ccc2fd3c077c5cb1b7cc0729b2
wouter <wouter@debruijn.dev>
2024-05-16 17:31:02 +0200

    feat(web): redesign login page

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -1,39 +1,50 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { Router, Routes } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { LucideAngularModule } from 'lucide-angular';
+import { ToastrService } from 'ngx-toastr';
+import { SpinnerComponent } from '../../components/spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule, FormsModule, LucideAngularModule, SpinnerComponent, CommonModule],
 })
 export class LoginComponent {
-  @ViewChild('login') login!: ElementRef<HTMLDivElement>;
-  @ViewChild('2fa') fa!: ElementRef;
-  @ViewChild('password') password!: ElementRef;
-  @ViewChild('username') username!: ElementRef;
-  @ViewChild('2facode') facode!: ElementRef;
+  form = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   showPassword = false;
 
-  constructor(private router: Router) {}
+  private auth = inject(AuthService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
 
-  onContinueClick(): void {
-    if (this.username.nativeElement.value === 'admin' && this.password.nativeElement.value === 'admin') {
-      this.login.nativeElement.style.display = 'none';
-      this.fa.nativeElement.hidden = false;
-    } else {
-      alert('Invalid credentials');
-    }
-  }
+  async onLoginClick(): Promise<void> {
+    this.form.disable();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  onLoginClick(): void {
-    if (this.facode.nativeElement.value === '123456') {
-      this.router.navigate(['/batches']);
-    } else {
-      alert('Invalid 2FA code');
+    const result = await this.auth.login(this.form.value.username ?? '', this.form.value.password ?? '');
+    if (result.success === false) {
+      console.log(result);
+      if (result.message === 'INVALID_FIELDS' && result.fields) {
+        this.form.enable();
+        this.form.setErrors(result.fields);
+      }
+      if (result.message === 'BAD_CREDENTIALS') {
+        this.toastr.error('Invalid username or password');
+        this.form.enable();
+      }
+
+      return;
     }
+
+    if (result.success) this.router.navigate(['/']);
   }
 
   toggleShowPassword(): void {
