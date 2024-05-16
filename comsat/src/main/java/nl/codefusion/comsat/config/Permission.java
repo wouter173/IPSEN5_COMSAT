daffbfb38aa1326bcb46f8ec5778afa497bf7306
RoKost14 <s1135548@student.hsleiden.nl>
2024-05-16 11:24:02 +0200

    Feat: Added new permissions

--- comsat/src/main/java/nl/codefusion/comsat/config/Permission.java
+++ comsat/src/main/java/nl/codefusion/comsat/config/Permission.java
@@ -2,23 +2,42 @@ package nl.codefusion.comsat.config;
 
 import lombok.Getter;
 
+@Getter
 public enum Permission {
-    EDIT(1),
-    READ_TEMPLATE(2),
-    EDIT_TEMPLATE(4),
-    MANAGE_USERS(8);;
+    CREATE_BATCH(1),
+    READ_BATCH(2),
+    DELETE_BATCH(4),
+    UPDATE_BATCH(8),
+    SEND_BATCH(16),
 
-    @Getter
+    CREATE_CONTACT(32),
+    READ_CONTACT(64),
+    READ_CONTACT_DETAILS(128),
+    UPDATE_CONTACT(256),
+    DELETE_CONTACT(512),
+
+    CREATE_TEMPLATE(1024),
+    READ_TEMPLATE(2048),
+    UPDATE_TEMPLATE(4096),
+    DELETE_TEMPLATE(8192),
+    SUGGEST_TEMPLATE(16384),
+
+    CREATE_USER(32768),
+    READ_USER(65536),
+    UPDATE_USER(131072),
+    DELETE_USER(262144),
+
+    READ_REPORT(524288),
+    EXPORT_REPORT(1048576);
+
     private final int value;
 
     Permission(int value) {
         this.value = value;
     }
-
-
 }
 
 
-//TODO: Add more permissions
+
 
 
