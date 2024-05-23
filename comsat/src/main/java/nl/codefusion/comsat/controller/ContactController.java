2c5daffcb09c430cdc4616921b6a3482414b5f65
bas <s1150223@student.hsleiden.nl>
2024-05-23 12:17:13 +0200

    feat: ContactController Requestmapping added

--- comsat/src/main/java/nl/codefusion/comsat/controller/ContactController.java
+++ comsat/src/main/java/nl/codefusion/comsat/controller/ContactController.java
@@ -7,16 +7,14 @@ import nl.codefusion.comsat.models.ContactModel;
 import nl.codefusion.comsat.repository.ContactRepository;
 import nl.codefusion.comsat.service.PermissionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.NoPermissionException;
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1")
 public class ContactController {
 
     private final ContactRepository contactRepository;
