9ef55017b2f1f566170633ede6a51398e943f7a4
bas <s1150223@student.hsleiden.nl>
2024-05-30 15:07:10 +0200

    feat: gray-out function works now, and with better visuals

--- web/src/app/components/batch-detail/batch-detail.component.ts
+++ web/src/app/components/batch-detail/batch-detail.component.ts
@@ -1,69 +1,82 @@
-import { Component, computed, effect, inject, Inject, Input, signal, Signal } from '@angular/core';
-import { BatchesService } from '../../services/batches.service';
-import { Batch } from '../../models/batch';
-import { LucideAngularModule } from 'lucide-angular';
-import { platforms } from '../../models/platform';
-import { SpinnerComponent } from '../spinner/spinner.component';
-import { minDelay, sleep } from '../../utils/mindelay';
+import {Component, computed, effect, inject, Inject, Input, signal, Signal} from '@angular/core';
+import {BatchesService} from '../../services/batches.service';
+import {Batch} from '../../models/batch';
+import {LucideAngularModule} from 'lucide-angular';
+import {platforms} from '../../models/platform';
+import {SpinnerComponent} from '../spinner/spinner.component';
+import {minDelay, sleep} from '../../utils/mindelay';
 import {Contact} from "../../models/contact";
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-batch-detail',
-  standalone: true,
-  templateUrl: './batch-detail.component.html',
-  imports: [LucideAngularModule, SpinnerComponent, FormsModule, CommonModule],
+    selector: 'app-batch-detail',
+    standalone: true,
+    templateUrl: './batch-detail.component.html',
+    styleUrl: './batch-detail.component.scss',
+    imports: [LucideAngularModule, SpinnerComponent, FormsModule, CommonModule],
 })
 export class BatchDetailComponent {
-  @Input() selectedBatchId!: Signal<string | null>;
-  public batchesService = inject(BatchesService);
-  public selectedBatch = computed(() => this.batchesService.batches().find((batch) => batch.id === this.selectedBatchId()));
-  public editingContact: Contact | null = null;
-  public platforms = platforms;
+    @Input() selectedBatchId!: Signal<string | null>;
+    public batchesService = inject(BatchesService);
+    public selectedBatch = computed(() => this.batchesService.batches().find((batch) => batch.id === this.selectedBatchId()));
+    public editingContact: Contact | null = null;
+    public platforms = platforms;
+    selectedContacts: Contact[] = [];
 
-  async onSendClick() {
-    const id = this.selectedBatchId();
-    if (!id) return;
-    this.batchesService.updateBatch(id, { state: 'SENDING' });
-    await sleep(1000);
+    async onSendClick() {
+        const id = this.selectedBatchId();
+        if (!id) return;
+        this.batchesService.updateBatch(id, {state: 'SENDING'});
+        await sleep(1000);
 
-    this.batchesService.updateBatch(id, {
-      contacts: this.selectedBatch()?.contacts.map((contact) => ({ ...contact, status: 'SENDING' })) ?? [],
-    });
+        this.batchesService.updateBatch(id, {
+            contacts: this.selectedBatch()?.contacts.map((contact) => ({...contact, status: 'SENDING'})) ?? [],
+        });
 
-    await sleep(1000);
+        await sleep(1000);
 
-    const contactStatus = ['SENT', 'ERROR', 'READ', 'REPLIED'];
-    this.batchesService.updateBatch(id, {
-      contacts:
-        this.selectedBatch()?.contacts.map((contact) => ({
-          ...contact,
-          status: contactStatus[Math.round(Math.random() * contactStatus.length)] as 'NOTSENT',
-        })) ?? [],
-    });
-    await sleep(2000);
-    this.batchesService.updateBatch(id, { state: 'SENT' });
+        const contactStatus = ['SENT', 'ERROR', 'READ', 'REPLIED'];
+        this.batchesService.updateBatch(id, {
+            contacts:
+                this.selectedBatch()?.contacts.map((contact) => ({
+                    ...contact,
+                    status: contactStatus[Math.round(Math.random() * contactStatus.length)] as 'NOTSENT',
+                })) ?? [],
+        });
+        await sleep(2000);
+        this.batchesService.updateBatch(id, {state: 'SENT'});
 
-    const batch = this.selectedBatch();
-    if (!batch) {
-      console.error('Batch not found');
-      return;
+        const batch = this.selectedBatch();
+        if (!batch) {
+            console.error('Batch not found');
+            return;
+        }
     }
-  } catch (error: any) {
-    console.error('Error sending batch', error);
-  }
 
-  onDeleteClick(contact: Contact) { //To delete the contact
-    console.log(contact.nickname + ' deleted');
-  }
+    catch(error: any) {
+        console.error('Error sending batch', error);
+    }
 
-  onEditClick(contact: Contact) { //To edit the contact
-    this.editingContact = contact;
-  }
+    onDeleteClick(contact: Contact) { //To delete the contact
+        console.log(contact.nickname + ' deleted');
+    }
 
-  onSaveClick() { //Click the save button in editing mode
-    console.log(this.editingContact?.nickname);
-    this.editingContact = null;
-  }
+    onEditClick(contact: Contact) { //To edit the contact
+        this.editingContact = contact;
+    }
+
+    grayOut(contact: Contact) {
+        const foundContact = this.selectedContacts.find(c => c.id === contact.id);
+        if (!foundContact) {
+            this.selectedContacts.push(contact);
+        } else {
+            this.selectedContacts = this.selectedContacts.filter(c => c.id !== contact.id);
+        }
+    }
+
+    onSaveClick() { //Click the save button in editing mode
+        console.log(this.editingContact?.nickname);
+        this.editingContact = null;
+    }
 }
