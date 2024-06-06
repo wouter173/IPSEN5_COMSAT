67c6471b71a8d4f69537d29c2991f1dbbcfc0f53
wouter <wouter@debruijn.dev>
2024-06-06 12:26:29 +0200

    feat(comsat): uncommited changes

--- comsat/src/main/java/nl/codefusion/comsat/dao/BatchContactEntryDao.java
+++ comsat/src/main/java/nl/codefusion/comsat/dao/BatchContactEntryDao.java
@@ -4,13 +4,15 @@ import lombok.RequiredArgsConstructor;
 import nl.codefusion.comsat.models.BatchContactEntryModel;
 import nl.codefusion.comsat.models.ContactModel;
 import nl.codefusion.comsat.repository.BatchContactEntryRepository;
-import nl.codefusion.comsat.repository.ContactRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BatchContactEntryDao {
-    private final ContactRepository contactRepository;
+    private final ContactDao contactDao;
     private final BatchContactEntryRepository batchContactEntryRepository;
 
     public BatchContactEntryModel create(BatchContactEntryModel contactToBatchModel) {
@@ -18,12 +20,17 @@ public class BatchContactEntryDao {
     }
 
     public BatchContactEntryModel updateBatchStatusByUsername(String nickName, String status) {
-        ContactModel contact = contactRepository.findContactByNickname(nickName);
+        ContactModel contact = contactDao.findByNickname(nickName);
 
         // TODO: Get Batch id and and don't use getFirst
         BatchContactEntryModel contactToBatch = contact.getBatchContacts().getFirst();
         contactToBatch.setStatus(status);
 
         return batchContactEntryRepository.save(contactToBatch);
+    }
+
+    public List<BatchContactEntryModel> findAllByBatchId(UUID batchId) {
+
+        return batchContactEntryRepository.findAllByBatchId(batchId);
     }
 }
