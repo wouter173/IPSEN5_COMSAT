88830943b07b87612c908b3cf8a8deb01c92014e
wouter <wouter@debruijn.dev>
2024-06-11 18:38:30 +0200

    chore: style changes

--- web/src/app/services/contacts.service.ts
+++ web/src/app/services/contacts.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, inject } from '@angular/core';
-import { Observable, from, map } from 'rxjs';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, from, map, tap } from 'rxjs';
 import { z } from 'zod';
 import { Contact } from '../models/contact';
 import { ContactWithBatch, contactWithBatchSchema } from '../models/contactwithbatch';
@@ -10,16 +10,25 @@ import { ApiService } from './api.service';
 })
 export class ContactsService {
   private api = inject(ApiService);
+  private _contacts = signal<ContactWithBatch[]>([]);
+  public contacts = this._contacts.asReadonly();
 
-  getContacts(): Observable<ContactWithBatch[]> {
-    return from(this.api.get('/contacts', { schema: z.array(contactWithBatchSchema) })).pipe(map((contacts) => contacts.data));
+  constructor() {
+    this.getContacts().subscribe();
   }
 
-  deleteContact(id: string): Observable<unknown> {
+  public getContacts(): Observable<ContactWithBatch[]> {
+    return from(this.api.get('/contacts', { schema: z.array(contactWithBatchSchema) })).pipe(
+      map((contacts) => contacts.data),
+      tap(this._contacts.set),
+    );
+  }
+
+  public deleteContact(id: string): Observable<unknown> {
     return from(this.api.delete(`/contacts/${id}`));
   }
 
-  updateContact(id: string, contact: Contact): Observable<unknown> {
+  public updateContact(id: string, contact: Contact): Observable<unknown> {
     return from(this.api.put(`/contacts/${id}`, { body: contact }));
   }
 }
