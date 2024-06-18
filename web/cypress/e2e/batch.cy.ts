c8c5776cc7da5b60d6b690f38a9ad8398aaf539d
RoKost14 <s1135548@student.hsleiden.nl>
2024-06-18 22:15:31 +0200

    Feat: added tests for frontend

---
+++ web/cypress/e2e/batch.cy.ts
@@ -0,0 +1,35 @@
+describe("Batch", () => {
+
+  it("Should send a Batch", () => {
+    cy.visit('/login');
+
+    cy.get('input[name="username"]').type('admin@gmail.com');
+    cy.get('input[name="password"]').type('admin');
+    cy.get('button[type="submit"]').click();
+    cy.wait(500);
+    cy.visit("/batches");
+
+
+    cy.get('[data-cy="batch-select"]').click();
+    cy.get('[data-cy="send-batch"]').click()
+    cy.wait(4000)
+
+    cy.get('[data-cy="sent"]').contains('Sent!').should('be.visible');
+  });
+
+  it("Should delete a contact", () => {
+    cy.visit('/login');
+    cy.get('input[name="username"]').type('admin@gmail.com');
+    cy.get('input[name="password"]').type('admin');
+    cy.get('button[type="submit"]').click();
+    cy.wait(500);
+    cy.visit("/batches");
+    cy.get('[data-cy="batch-select"]').click();
+
+    cy.get('[data-cy="delete-contact"]').first().click()
+
+
+  });
+
+
+});
--- web/cypress/support/commands.ts
+++ web/cypress/support/commands.ts
@@ -1,37 +1,22 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+Cypress.Commands.add(
+  'bypassLogin',
+  {
+    prevSubject: 'optional',
+  },
+  () => {
+    cy.session([], () => {
+      cy.intercept('http://localhost:8080/api/v1/auth/login', {
+        fixture: 'login-success.json',
+      });
+      cy.visit('/login');
+      cy.get('input[name="username"]').type('a');
+      cy.get('input[name="password"]').type('b');
+      cy.get('button[type="submit"]').click();
+      cy.wait(500);
+      // cy.get('.toast-message').should('be.visible');
+      cy.location().should((location) => {
+        expect(location.pathname).to.equal('/');
+      });
+    });
+  }
+);
